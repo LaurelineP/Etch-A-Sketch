d1e96cb598d3ab43ccd023fad96301a4591ea49c
Laureline <laurelinedev@gmail.com>
2019-02-10 21:13:23 +0100

    All buttons working and controlled

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -23,47 +23,55 @@ function generateGrid (n) {
         settupCSS(n)
         return allSquares;
     }
-    $('#grid-size-input').attr('placeholder',`${n} (x ${n})`);
+    $('#grid-input').attr('placeholder',`${n} (x ${n})`);
     return generateSquares( n )
 }
 
 
 /* Check the input's value for digits and not alphabetical caracters */
 function check () {
-    let inputValue = $('#grid-size-input')[0].value;
+    let inputValue = event.target.value;
     let regexp = /^\d{1,}$/;
-    if(inputValue && !inputValue.match(regexp)){
-        $('#errorMessage').addClass('visible');
-        return false;
-    } else {
+    if(inputValue){
+        if(!inputValue.match(regexp) || inputValue < 16){
+            $('#errorMessage').addClass('visible');
+            return false;
+        } else {
+            $('#errorMessage').removeClass('visible');
+            return true;
+        }
+    } else if ( inputValue === '') {
         $('#errorMessage').removeClass('visible');
-        return true;
     }
+    return null;
 }
 
 
 /* Submitting the input or Make on action on Add or Remove buttons */
 function changeGridSize ( action ) {
-    let operation = action === 'more' ? +4 : action === 'less' ? -4 : getValue();
-    console.log('operation', operation)
+    let inputValue = parseInt($('#grid-input')[0].value);
+    initValue = action === 'more' ? initValue + 4 : action === 'less' ? initValue - 4 : inputValue;
+    if( initValue >= 16) {
+        $('#grid-input')[0].value = '';
+        $('#grid').append( Grid(initValue) )
+    } else {
+        initValue = 16;
+    }
 
-    function getValue(){
-        let value = $('#grid-size-input')[0].value;
-        if(check()){
-            remove()
-            console.log('operation', operation)
-            $('#grid').append( Grid(value) )
-        }
+}
+
+/* Submitting by pressink key "Enter" */
+function getEnter () {
+    if(event.charCode === 13 && check()){
+        changeGridSize( 'getValue' )
     }
 }
 
-
-function remove() {
+function remove () {
     $('.square').remove();
 }
 
 Grid = (n) => {
-    console.log(' hello')
     let initCols = n ? n : initValue;
     remove()
     $('#grid').append( generateGrid(initCols) );
