d93d836a5802cc9c8a01e9b0bc67a3c25dbb256e
Laureline <laurelinedev@gmail.com>
2019-02-10 12:43:23 +0100

    Create Grid improved : input + 2 buttons (--> not quite finished ) + clear button ?

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -1,26 +1,72 @@
+let initValue = 16;
+/* Init grid creation */
+function generateGrid (n) {
+    /* Set value of css propriety grid-template-columns */
+    function settupCSS (n) {
+        var nCol = n;
+        let classValue = '';
+        while ( nCol > 0){
+            classValue += '30px '
+            nCol--;
+        }
+        $('#grid').css({"display":"grid","grid-template-columns":classValue});
+    }
 
-function Grid (n) {
-    createGridTemplateCol(n)
-    const generateSquares = function(n) {
+    /* Generate squares */
+    function generateSquares (n) {
         let nSquares = n * n;
         let allSquares = '';
         while (nSquares > 0) {
             allSquares += `<div id="${nSquares}" class="square"></div>`
             nSquares--;
         }
+        settupCSS(n)
         return allSquares;
     }
-    const grid = generateSquares(n);
-    return grid;
+    $('#grid-size-input').attr('placeholder',`${n} (x ${n})`);
+    return generateSquares( n )
 }
 
 
-function createGridTemplateCol (n) {
-    let className = '';
-    while ( n > 0){
-        className += 'auto '
-        n--;
+/* Check the input's value for digits and not alphabetical caracters */
+function check () {
+    let inputValue = $('#grid-size-input')[0].value;
+    let regexp = /^\d{1,}$/;
+    if(inputValue && !inputValue.match(regexp)){
+        $('#errorMessage').addClass('visible');
+        return false;
+    } else {
+        $('#errorMessage').removeClass('visible');
+        return true;
     }
-    $('#grid').css({"display":"grid","grid-template-columns":className});
 }
-$('#grid').append(Grid(16));
+
+
+/* Submitting the input or Make on action on Add or Remove buttons */
+function changeGridSize ( action ) {
+    let operation = action === 'more' ? +4 : action === 'less' ? -4 : getValue();
+    console.log('operation', operation)
+
+    function getValue(){
+        let value = $('#grid-size-input')[0].value;
+        if(check()){
+            remove()
+            console.log('operation', operation)
+            $('#grid').append( Grid(value) )
+        }
+    }
+}
+
+
+function remove() {
+    $('.square').remove();
+}
+
+Grid = (n) => {
+    console.log(' hello')
+    let initCols = n ? n : initValue;
+    remove()
+    $('#grid').append( generateGrid(initCols) );
+}
+
+$(document).ready( () => Grid());
